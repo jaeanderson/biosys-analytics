3f694dbd258e98ba6b18611aa4b7ad2f19cc6720
jaeanderson <jaea@email.arizona.edu>
2019-02-22 17:31:56 -0700

    homework 5 grad

--- assignments/05-python-tictactoe/outcome.py
+++ assignments/05-python-tictactoe/outcome.py
@@ -47,30 +47,18 @@ def main():
     if not re.search('^[.XO]{9}$',state_outcome):
         die('State "{}" must be 9 characters of only ., X, O'.format(state_outcome))
 
-
     state_translate = ''
 
-    if win_dict.get(state_outcome) != None:
-        die('{} has won'.format(win_dict.get(state_outcome)))
-
     for move, player in win_dict.items():
-        #print('move: {} player: {}'.format(move,player))
         for i, char in enumerate(state_outcome, start=1):
             if char == move[i-1]:
                 state_translate += str(char)
             else:
                 state_translate += '.'
-        #print(state_translate)
         if state_translate == move:
-            #print('HELLO')
             die('{} has won'.format(win_dict.get(state_translate)))
-        #else:
-        #    die('No winner')
         state_translate = ''
     die('No winner')
-
-
-
 
 #---------------------------------------------------------
 if __name__ == '__main__':
