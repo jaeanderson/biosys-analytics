b8514d6edccc6ed9b24f1006a9d8358d5c0408a7
jaeanderson <jaea@email.arizona.edu>
2019-03-26 11:16:51 -0700

    homework 10

--- assignments/10-war/war.py
+++ assignments/10-war/war.py
@@ -50,7 +50,7 @@ def main():
     seed = args.seed
 
     suits = ['\u2660','\u2663','\u2665','\u2666']
-    cvalues = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+    cvalues = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 
     cdeck = (list(itertools.product(suits, cvalues)))
     #print(sorted(cdeck))
@@ -60,16 +60,30 @@ def main():
     for i, card in enumerate(d):
         vmod = i % 13
         deck_dict[card] = vmod + 1
-
     
     card_list = sorted(deck_dict.keys())
     random.seed(seed)
     random.shuffle(card_list)
     #print(card_list)
 
-    p1 = []; p2 = []
     p1_ctr = 0; p2_ctr = 0; card_ctr = 0
     while True:
+        p1_card = card_list.pop()
+        p1_value = deck_dict[p1_card]
+
+        p2_card = card_list.pop()
+        p2_value = deck_dict[p2_card]
+        
+        if p1_value > p2_value:
+            p1_ctr += 1
+            win = 'P1'
+        if p1_value < p2_value:
+            p2_ctr += 1
+            win = 'P2'
+        if p1_value == p2_value:
+            win = 'WAR!'
+        print('{:>3} {:>3} {:<4}'.format(p1_card, p2_card, win))
+
         if len(card_list) == 0:
             if p1_ctr > p2_ctr:
                 game_winner = 'Player 1 wins'
@@ -79,22 +93,6 @@ def main():
                 game_winner = 'DRAW'
             print('P1 {} P2 {}: {}'.format(p1_ctr, p2_ctr, game_winner))
             break
-
-        p1_card = card_list.pop()
-        p1_value = deck_dict[p1_card]
-        
-        p2_card = card_list.pop()
-        p2_value = deck_dict[p2_card]
-        
-        if p1_value > p2_value:
-            p1_ctr += 1
-            win = 'P1'
-        elif p1_value < p2_value:
-            p2_ctr += 1
-            win = 'P2'
-        else:
-            win = 'WAR!'
-        print('{:>3} {:>3} {:<4}'.format(p1_card, p2_card, win))
 
 #---------------------------------------------------------
 if __name__ == '__main__':
